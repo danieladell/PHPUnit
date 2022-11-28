770e771812205c5945452c6d89e716a0b7c845e9
danieladell <danieladell1999@gmail.com>
2022-11-28 18:52:33 +0100

    Añadir test 1

---
+++ src/Model/User.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Model;
+
+class User {
+
+    public function __construct(int $age) {
+        $this->age = $age;
+        
+    }
+
+    public function isAnAdult(): bool{
+        return $this->age >= 18;
+
+    }
+}
