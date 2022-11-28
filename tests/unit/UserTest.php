66a6e1ec9f6b4b0a860873f2419a47ea447f6766
danieladell <danieladell1999@gmail.com>
2022-11-28 19:04:33 +0100

    Añadir tests 1 y 2

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -6,9 +6,27 @@ use Model\User;
 
 class UserTest extends TestCase {
     
-    public function testUserIsAnAdultReturnsFalseForMinors(){
+    public function testUserIsAnAdultReturnsFalseForMinors() {
         $user = new User(17);
         $this->assertEquals(false, $user->IsAnAdult());
+        
+    }
+
+    public function testUserIsAnAdultReturnsTrueForPeopleOlderThanThresshold() {
+        $user = new User(19);
+        $this->assertTrue($user->IsAnAdult());
+
+        $user = new User(54);
+        $this->assertTrue($user->IsAnAdult());
+
+        $user = new User(111);
+        $this->assertTrue($user->IsAnAdult());
+
+    }
+
+    public function testUserIsAnAdultReturnsTrueForThresshold() {
+        $user = new User(18);
+        $this->assertTrue($user->IsAnAdult());
 
     }
 }
